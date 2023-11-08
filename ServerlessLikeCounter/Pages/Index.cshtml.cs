ecca4261efefb25c021931eb0c31ce39671c1a56
Saphiroths <yarik@hetblomhof.nl>
2023-11-08 12:26:58 +0100

    Testing Serverless computing
    Made a basic button that adds 1.
    Published to Azure.

---
+++ ServerlessLikeCounter/Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ServerlessLikeCounter.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        [BindProperty]
+        public int Counter { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Counter = 0;
+        }
+
+        public IActionResult OnPost()
+        {
+            Counter++;
+            return Page();
+        }
+    }
+}
