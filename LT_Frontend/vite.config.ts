c6dbec658612b0b18e1a2235f0c772eb12cda752
Saphiroths <yarik@hetblomhof.nl>
2023-12-23 14:01:00 +0100

    added dockerfile to frontend

--- LT_Frontend/vite.config.ts
+++ LT_Frontend/vite.config.ts
@@ -17,5 +17,13 @@ export default defineConfig({
         about: resolve(root, 'about', 'index.html'),
       }
     }
+  },
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: true, // needed for the Docker Container port mapping to work
+    strictPort: true,
+    port: 5173, // you can replace this port with any port
   }
 })
